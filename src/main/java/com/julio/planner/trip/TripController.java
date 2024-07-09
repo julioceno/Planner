508210c90b3c7e6864f37a670f7ab781dba1c062
Júlio Nepomuceno <julioreplay@gmail.com>
2024-07-08 21:52:01 -0300

    first commit

---
+++ src/main/java/com/julio/planner/trip/TripController.java
@@ -0,0 +1,35 @@
+package com.julio.planner.trip;
+
+import com.julio.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository repository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload tripRequestPayload) {
+        Trip newTrip = new Trip(tripRequestPayload);
+
+        repository.save(newTrip);
+        participantService.registerParticipantsToEvent(tripRequestPayload.emails_to_invite(), newTrip.getId());
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
+        Optional<Trip> trip = repository.findById(id);
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
